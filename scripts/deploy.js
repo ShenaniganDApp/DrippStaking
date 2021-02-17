f43b75c2ce6094c47d85bc9b8c0a6feddba73a9b
YoungKidWarrior <ginelliunit@gmail.com>
2021-02-17 02:48:58 -0800

    Token Contracts

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,36 +1,108 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional 
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
-const hre = require("hardhat");
+/* eslint no-use-before-define: "warn" */
+const fs = require('fs');
+const chalk = require('chalk');
+const { config, ethers } = require('hardhat');
+const { utils, BigNumber } = require('ethers');
+const R = require('ramda');
 
-async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile 
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+const main = async () => {
+	console.log('\n\n 📡 Deploying...\n');
 
-  // We get the contract to deploy
-  const DrippStaking = await hre.ethers.getContractFactory("Dripp Staking");
-  const ERC20 = await hre.ethers.getContractFactory("ERC20");
-  const drippStaking = await DrippStaking.deploy();
-  const erc20Capped= await ERC20.deploy("Shweatpants", "SHWEATPANTS");
+	// const shweatpantsERC20 = await deploy('ShweatpantsToken', [BigNumber.from('100000000000000000000')]); // <-- add in constructor args like line 19 vvvv
+	const agaaveERC20 = await deploy('AlvinToken', [BigNumber.from('100000000000000000000')]); // <-- add in constructor args like line 19 vvvv
+	// const drippStaking = await deploy('DrippStaking'); // <-- add in constructor args like line 19 vvvv
 
-  await drippStaking.deployed();
-  await erc20Capped.deployed();
+	//const secondContract = await deploy("SecondContract")
 
-  console.log("Dripp Staking deployed to:", drippStaking.address);
-  console.log("ERC20 deployed to:", erc20Capped.address);
+	// const exampleToken = await deploy("ExampleToken")
+	// const examplePriceOracle = await deploy("ExamplePriceOracle")
+	// const smartContractWallet = await deploy("SmartContractWallet",[exampleToken.address,examplePriceOracle.address])
+
+	/*
+  //If you want to send value to an address from the deployer
+  const deployerWallet = ethers.provider.getSigner()
+  await deployerWallet.sendTransaction({
+    to: "0x34aA3F359A9D614239015126635CE7732c18fDF3",
+    value: ethers.utils.parseEther("0.001")
+  })
+  */
+
+	/*
+  //If you want to send some ETH to a contract on deploy (make your constructor payable!)
+  const yourContract = await deploy("YourContract", [], {
+  value: ethers.utils.parseEther("0.05")
+  });
+  */
+
+	/*
+  //If you want to link a library into your contract:
+  // reference: https://github.com/austintgriffith/scaffold-eth/blob/using-libraries-example/packages/hardhat/scripts/deploy.js#L19
+  const yourContract = await deploy("YourContract", [], {}, {
+   LibraryName: **LibraryAddress**
+  });
+  */
+
+	console.log(
+		' 💾  Artifacts (address, abi, and args) saved to: ',
+		chalk.blue('packages/hardhat/artifacts/'),
+		'\n\n'
+	);
+};
+
+const deploy = async (contractName, _args = [], overrides = {}, libraries = {}) => {
+	console.log(` 🛰  Deploying: ${contractName}`);
+
+	const contractArgs = _args || [];
+	const contractArtifacts = await ethers.getContractFactory(contractName, { libraries: libraries });
+	const deployed = await contractArtifacts.deploy(...contractArgs, overrides);
+	const encoded = abiEncodeArgs(deployed, contractArgs);
+	fs.writeFileSync(`artifacts/${contractName}.address`, deployed.address);
+
+	console.log(' 📄', chalk.cyan(contractName), 'deployed to:', chalk.magenta(deployed.address));
+
+	if (!encoded || encoded.length <= 2) return deployed;
+	fs.writeFileSync(`artifacts/${contractName}.args`, encoded.slice(2));
+
+	return deployed;
+};
+
+// ------ utils -------
+
+// abi encodes contract arguments
+// useful when you want to manually verify the contracts
+// for example, on Etherscan
+const abiEncodeArgs = (deployed, contractArgs) => {
+	// not writing abi encoded args if this does not pass
+	if (!contractArgs || !deployed || !R.hasPath(['interface', 'deploy'], deployed)) {
+		return '';
+	}
+	const encoded = utils.defaultAbiCoder.encode(deployed.interface.deploy.inputs, contractArgs);
+	return encoded;
+};
+
+// checks if it is a Solidity file
+const isSolidity = (fileName) =>
+	fileName.indexOf('.sol') >= 0 && fileName.indexOf('.swp') < 0 && fileName.indexOf('.swap') < 0;
+
+const readArgsFile = (contractName) => {
+	let args = [];
+	try {
+		const argsFile = `./contracts/${contractName}.args`;
+		if (!fs.existsSync(argsFile)) return args;
+		args = JSON.parse(fs.readFileSync(argsFile));
+	} catch (e) {
+		console.log(e);
+	}
+	return args;
+};
+
+function sleep(ms) {
+	return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main()
-  .then(() => process.exit(0))
-  .catch(error => {
-    console.error(error);
-    process.exit(1);
-  });
+	.then(() => process.exit(0))
+	.catch((error) => {
+		console.error(error);
+		process.exit(1);
+	});
